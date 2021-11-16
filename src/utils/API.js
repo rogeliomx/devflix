9b37525b0344b56a2e551eaf41acef0666e853bb
Rogelio Galindo <roge.galindo@gmail.com>
2021-11-15 18:48:12 -0600

    chore: https api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const searchShow= (search) => {
-        return axios.get(`http://api.tvmaze.com/search/shows?q=${search}`)
+        return axios.get(`https://api.tvmaze.com/search/shows?q=${search}`)
     }
 
 const searchCast = () => {
